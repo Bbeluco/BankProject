4b99e9b1dc7e4b442993abfd311375062c8432da
Bruno Beluco <beluco.bispo@hotmail.com>
2024-03-10 14:05:21 -0300

    Backend 3/10 completed

---
+++ BankTests/RangeDateAttributeTest.cs
@@ -0,0 +1,21 @@
+﻿using BankProject;
+
+namespace BankTests;
+
+[TestClass]
+public class RangeDateAttributeTest
+{
+
+    [TestMethod]
+    public void RangeDateAttribute_PassValidDate_ReturnsValid() {
+        RangeDateAttribute range = new RangeDateAttribute(-100);
+        Assert.IsTrue(range.IsValid(DateTime.UtcNow.AddDays(-1)));
+    }
+
+    [TestMethod]
+    public void RangeDateAttribute_PassInvalidDate_ReturnsInvalid() {
+        RangeDateAttribute range = new RangeDateAttribute(-100);
+        Assert.IsFalse(range.IsValid(DateTime.UtcNow.AddDays(1)));
+    }
+
+}
